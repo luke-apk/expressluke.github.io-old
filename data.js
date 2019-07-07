6177f2049e0bf7d3b6fdcab9ef6690d34dc19793
Luke Williams <expressluke@turbox.uk>
2019-07-07 19:16:32 +0100

    Redesign

---
+++ data.js
@@ -0,0 +1,91 @@
+var data = [{
+  "name":"Resurrection Remix",
+  "herolink":"https://get.resurrectionremix.com/?dir=herolte",
+  "hero2link":"https://get.resurrectionremix.com/?dir=hero2lte",
+  "md5hero":"fbf8c4536b6a729781ec8d8fce4ea235",
+  "md5hero2":"f86c4a389387fa2a0c553252aa949d55",
+  "xdahero":"https://forum.xda-developers.com/galaxy-s7/development/9-0-resurrection-remix-v7-0-t3891339",
+  "xdahero2":"https://forum.xda-developers.com/s7-edge/development/9-0-resurrection-remix-v7-0-t3891229",
+  "telegram":"https://t.me/resurrectionremixchat",
+  "website":"https://www.resurrectionremix.com/",
+  "changelog":[
+    {"version":"v7.0.2-20190706", "changes":["July patch merged", "NFC hal prop replaced (Should resolve nfc turning on issues)", "AudioFX gone once again!", "Misc stuff blah blah blah all the usuals"]},
+    {"version":"v7.0.2-20190605", "changes":["Updated vendor May security patch", "Removed unused gralloc usage bits flag", "Misc performance and stability changes"]},
+    {"version":"v7.0.2-20190510", "changes":["May Security Patch Merged", "Linux 3.18.139"]}
+  ]
+}, {
+  "name":"Evolution-X",
+  "herolink":"https://drive.google.com/a/zonc.eu/uc?id=1jkgpUovNYp5Z8WesoRzSPhBhpOrw9P6k&export=download",
+  "hero2link":"https://drive.google.com/a/zonc.eu/uc?id=1KRvLQhY4TY-ysLb93yJXxVfHXFi1TMDq&export=download",
+  "md5hero":null,
+  "md5hero2":null,
+  "xdahero":null,
+  "xdahero2":null,
+  "telegram":"https://t.me/EvolutionXNews",
+  "website":"https://evolutionx.ml/",
+  "changelog":[
+    {"version":"0.0", "changes":["a", "b"]},
+    {"version":"0.0", "changes":["a", "b"]},
+    {"version":"0.0", "changes":["a", "b"]}
+  ]
+}, {
+  "name":"Clean Open Source Project",
+  "herolink":"https://mirror.codebucket.de/cosp/herolte",
+  "hero2link":"https://mirror.codebucket.de/cosp/hero2lte",
+  "md5hero":"f5c2f4fad1ab7ca6b6024b38047e3eaa",
+  "md5hero2":"5100e9ace44233f13f92dedad1c9150f",
+  "xdahero":"https://forum.xda-developers.com/galaxy-s7/development/rom-cosp-t3892551",
+  "xdahero2":"https://forum.xda-developers.com/s7-edge/development/rom-cosp-t3892527",
+  "telegram":"https://t.me/cospcommunity",
+  "website":"https://github.com/cosp-project"
+}, {
+  "name":"Pixel Experience",
+  "herolink":"https://download.pixelexperience.org/herolte/",
+  "hero2link":"https://download.pixelexperience.org/hero2lte/",
+  "md5hero":null,
+  "md5hero2":null,
+  "xdahero":"https://forum.xda-developers.com/galaxy-s7/development/rom-pixel-experience-t3773987",
+  "xdahero2":"https://forum.xda-developers.com/s7-edge/development/rom-pixel-experience-t3773983",
+  "telegram":"https://t.me/pixelexperiencechat",
+  "website":"https://download.pixelexperience.org/"
+}, {
+  "name":"crDroid",
+  "herolink":"https://crdroid.net/herolte",
+  "hero2link":"https://crdroid.net/hero2lte",
+  "md5hero":null,
+  "md5hero2":null,
+  "xdahero":"https://forum.xda-developers.com/galaxy-s7/development/crdroid-4-0-herolte-t3750760",
+  "xdahero2":"https://forum.xda-developers.com/s7-edge/development/crdroid-4-0-hero2lte-t3750750",
+  "telegram":"https://t.me/crDroidAndroid",
+  "website":"https://crdroid.net/"
+}, {
+  "name":"ViperOS",
+  "herolink":"https://sourceforge.net/projects/viper-project/files/herolte/",
+  "hero2link":"https://sourceforge.net/projects/viper-project/files/hero2lte/",
+  "md5hero":"ed7adc3fefdc2f6eb89f75ca3146accf",
+  "md5hero2":"46f0d0b62e7c2bc11ca12f5d166c1494",
+  "xdahero":"https://forum.xda-developers.com/galaxy-s7/development/9-0-viperos-t3902922",
+  "xdahero2":"https://forum.xda-developers.com/s7-edge/development/9-0-viper-os-t3901902",
+  "telegram":"https://t.me/viperos_chat",
+  "website":"https://sourceforge.net/projects/viper-project/"
+}, {
+  "name":"AOSP Extended",
+  "herolink":"https://drive.google.com/a/zonc.eu/uc?id=1vpapZr54S0RvzQqznMuKzwY8kLm6LAXS&export=download",
+  "hero2link":"https://drive.google.com/a/zonc.eu/uc?id=1peKMxmQK_IcJ7F2c5s4O-Hda4JqFAVck&export=download",
+  "md5hero":null,
+  "md5hero2":null,
+  "xdahero":"https://forum.xda-developers.com/galaxy-s7/development/rom-aospextended-rom-v6-4-t3923082",
+  "xdahero2":"https://forum.xda-developers.com/s7-edge/development/rom-aospextended-rom-v6-4-t3923068",
+  "telegram":"https://t.me/aospextendedgroup",
+  "website":"https://www.aospextended.com/"
+}, {
+  "name":"SuperiorOS",
+  "herolink":"https://sourceforge.net/projects/developerluke-roms/files/Superior-OS/herolte/",
+  "hero2link":"https://sourceforge.net/projects/developerluke-roms/files/Superior-OS/hero2lte/",
+  "md5hero":"e2212ebda7e5ef2d1dcf06a11fcdc1a4",
+  "md5hero2":"37ae9ebf02ab8375236408de0b0af78a",
+  "xdahero":"https://forum.xda-developers.com/galaxy-s7/development/rom-superioros-destructor-s7-t3897976",
+  "xdahero2":"https://forum.xda-developers.com/s7-edge/development/rom-superioros-destructor-s7-edge-t3897975",
+  "telegram":"https://t.me/superioros",
+  "website":"https://sourceforge.net/projects/superioros/"
+}];
